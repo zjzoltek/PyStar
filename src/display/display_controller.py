71753b851477eaea247d6633270f747ff75a6f9d
Zachary Zoltek <zoltekz@yahoo.com>
2024-10-07 08:48:06 -0500

    Updates

--- src/display/display_controller.py
+++ src/display/display_controller.py
@@ -3,15 +3,49 @@ from colorama import just_fix_windows_console
 
 import a_star
 from logging import Logger
+from typing import ClassVar
+from collections.abc import Iterable
 
 class DisplayController:
-    _is_initialized = False
+    _is_initialized: ClassVar[bool] = False
     
+    @staticmethod
+    def _is_valid_dimension(t: Iterable[int]) -> Iterable[str]:
+        validation_errors: list[str] = []
+        
+        if len(t) != 2:
+            validation_errors.append(f'Expected two arguments, got: {t}')
+            return validation_errors
+        
+        if t[0] <= 0:
+            validation_errors.append(f'Expected width to be a positive integer, got {t[0]}')
+        
+        if t[1] <= 0:
+            validation_errors.append(f'Expected height to be a positive integer, got {t[1]}')
+        
+        return validation_errors
+    
+    @staticmethod
+    def _request_dimensions_from_user(prompt: str) -> tuple[int, int]:
+        while True:
+            try:
+                dimensions = input(f'{prompt}? Seperate width and height by space eg. 1200 800 =>').split(' ')
+                validation_errors = DisplayController._is_valid_dimension(dimensions)
+                if validation_errors:
+                    
+                    print('Cell width and height must be two positive integers separated by a space')
+                    continue
+                    
+                return dimensions
+            except ValueError as e:
+                print(f'Error: {e}')
+                continue
+
     def __init__(self):
-        self._win_w = None
-        self._win_h = None
-        self._cell_w = None
-        self._cell_h = None
+        self._win_w: int = 0
+        self._win_h: int = 0
+        self._cell_w: int = 0
+        self._cell_h: int = 0
     
     def initialize(self):
         if self._is_initialized:
@@ -22,7 +56,11 @@ class DisplayController:
         self._is_initialized = True
 
     def get_display_dimensions():
-        
+        return DisplayController._request_dimensions_from_user(prompt='Window dimensions')
+    
+    def get_cell_dimensions():
+        return DisplayController._request_dimensions_from_user(prompt='Cell dimensions')\
+            
     def display_user_manual():
         print('Controls:')
         print('m - Re-Generate Maze')
@@ -32,22 +70,8 @@ class DisplayController:
         print('x - Clear path, but not start and end colors')
         print('z - Toggle drawboard')
 
-    def _request_dimensions_from_user():
-        while True:
-            try:
-                dimensions = input('Window size? Seperate width and height by space eg. 1200 800 =>')
-                (w, h) = dimensions.split()
-                if len(dims) != 2:
-                    Logger.warning('Incorrect number of args. Try again')
-                    print('\n')
-                    continue
-                else:
-                    w, h = int(dims[0]), int(dims[1])
-                    if w * h == 0:
-                        print('Cell width and height must be non-zero')
-            except ValueError as e:
-                print()
-                continue
+
+    
 def main():
     just_fix_windows_console()
     
